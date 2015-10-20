3b0614d52273e1ad3f5c6af8984f9ba729b9c5f2
Aram Kocharyan <akarmenia@gmail.com>
2015-10-20 14:02:22 +1100

    Change to urbanetic:utility

--- package.js
+++ package.js
@@ -18,11 +18,11 @@ Package.on_use(function(api) {
     'aldeed:simple-schema@1.3.0',
     'aramk:checkbox@0.1.0',
     'aramk:q@1.0.1_1',
-    'aramk:utility@0.9.0',
+    'aramk:routes@0.2.0',
     'digilord:roles@1.2.12',
     'matb33:collection-hooks@0.7.6',
     'reactive-var@1.0.5',
-    'aramk:routes@0.2.0'
+    'urbanetic:utility@1.0.1'
   ], ['client', 'server']);
   api.use([
     'templating',
